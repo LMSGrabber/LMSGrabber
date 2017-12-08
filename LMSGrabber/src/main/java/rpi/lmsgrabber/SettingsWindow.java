0bd3c76fbc5614a4556bc1c0059afa22743fb7a0
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-12-08 02:30:35 -0500

    Add dialog buttons

--- LMSGrabber/src/main/java/rpi/lmsgrabber/SettingsWindow.java
+++ LMSGrabber/src/main/java/rpi/lmsgrabber/SettingsWindow.java
@@ -2,31 +2,46 @@ package rpi.lmsgrabber;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.DialogPane;
 import javafx.stage.Modality;
+import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("restriction")
 public class SettingsWindow extends Stage {
+  @FXML // fx:id="input_proxy"
+  private TextField input_proxy; // Value injected by FXMLLoader
+
   public SettingsWindow(Stage parent) throws IOException {
-    super(StageStyle.DECORATED);
+    super(StageStyle.UNDECORATED);
+//    super();
     // Load root layout from fxml file.
     FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/settings_menu.fxml"));
 
     loader.setController(this);
 
-    System.out.println(parent);
+//    System.out.println(parent);
 
     this.initModality(Modality.WINDOW_MODAL);
     this.initOwner(parent);
     this.setAlwaysOnTop(true);
+    System.out.println("Doing the thing");
 
-    TabPane layout = loader.load();
+    DialogPane layout = loader.load();
+
+//    this.getScene().setRoot(layout);
+//    layout.setScene(this.getScene());
 
     Scene scene2 = new Scene(layout);
     this.setScene(scene2);
