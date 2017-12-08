ec14feb6bf86545e9583f6b2bae926bd2446a0e5
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-12-08 03:23:32 -0500

    Load default values from settings

--- LMSGrabber/src/main/java/rpi/lmsgrabber/SettingsWindow.java
+++ LMSGrabber/src/main/java/rpi/lmsgrabber/SettingsWindow.java
@@ -1,14 +1,17 @@
 package rpi.lmsgrabber;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.DialogPane;
 import javafx.stage.Modality;
@@ -22,11 +25,22 @@ import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings("restriction")
 public class SettingsWindow extends Stage {
+  private Preferences prefs = Preferences.userNodeForPackage(App.class);
+
+  @FXML // fx:id="min_delay_slider"
+  private Slider min_delay_slider; // Value injected by FXMLLoader
+
+  @FXML // fx:id="max_delay_slider"
+  private Slider max_delay_slider; // Value injected by FXMLLoader
+
   @FXML // fx:id="input_proxy"
   private TextField input_proxy; // Value injected by FXMLLoader
 
   @FXML // fx:id="settings_cancel"
   private TextField settings_cancel_btn; // Value injected by FXMLLoader
+
+  @FXML // fx:id="multithreaded_check"
+  private CheckBox multithreaded_check; // Value injected by FXMLLoader
 
   public SettingsWindow(Stage parent) throws IOException {
     super(StageStyle.UNDECORATED);
@@ -52,6 +66,11 @@ public class SettingsWindow extends Stage {
     this.setScene(scene2);
 
     this.setTitle("LMSGrabber Settings");
+
+    min_delay_slider.setValue(prefs.getDouble("min_delay", 1.0));
+    max_delay_slider.setValue(prefs.getDouble("max_delay", 3.0));
+    input_proxy.setText(prefs.get("proxy", ""));
+    multithreaded_check.setSelected(prefs.getBoolean("multithreaded", true));
   }
 
   @FXML
