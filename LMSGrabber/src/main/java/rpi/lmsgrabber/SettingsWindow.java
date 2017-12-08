85b8147130c59870cc499c97a5dfe1ba8d7e86dd
Colin Atkinson <colin.william.atkinson@gmail.com>
2017-12-08 02:58:02 -0500

    Implement cancel button

--- LMSGrabber/src/main/java/rpi/lmsgrabber/SettingsWindow.java
+++ LMSGrabber/src/main/java/rpi/lmsgrabber/SettingsWindow.java
@@ -2,12 +2,14 @@ package rpi.lmsgrabber;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.DialogPane;
 import javafx.stage.Modality;
 import javafx.stage.PopupWindow;
@@ -23,6 +25,9 @@ public class SettingsWindow extends Stage {
   @FXML // fx:id="input_proxy"
   private TextField input_proxy; // Value injected by FXMLLoader
 
+  @FXML // fx:id="settings_cancel"
+  private TextField settings_cancel_btn; // Value injected by FXMLLoader
+
   public SettingsWindow(Stage parent) throws IOException {
     super(StageStyle.UNDECORATED);
 //    super();
@@ -36,9 +41,9 @@ public class SettingsWindow extends Stage {
     this.initModality(Modality.WINDOW_MODAL);
     this.initOwner(parent);
     this.setAlwaysOnTop(true);
-    System.out.println("Doing the thing");
+//    System.out.println("Doing the thing");
 
-    DialogPane layout = loader.load();
+    TabPane layout = loader.load();
 
 //    this.getScene().setRoot(layout);
 //    layout.setScene(this.getScene());
@@ -47,5 +52,10 @@ public class SettingsWindow extends Stage {
     this.setScene(scene2);
 
     this.setTitle("LMSGrabber Settings");
+  }
+
+  @FXML
+  void onBtnClickCancel(ActionEvent event) {
+    this.close();
   }
 }
